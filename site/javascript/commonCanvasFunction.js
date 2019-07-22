2ddf39780d38b6a683b6aa17395c153543696a8a
wlee0515 <wlee0515@users.noreply.github.com>
2019-07-22 07:29:47 -0400

    fixes

--- site/javascript/commonCanvasFunction.js
+++ site/javascript/commonCanvasFunction.js
@@ -1,5 +1,3 @@
-<!--
-
 function resizeCanvas(iCanvas) {
   iCanvas.width = iCanvas.parentNode.clientWidth - 2;
   iCanvas.height = iCanvas.parentNode.clientHeight - 2;
@@ -33,8 +31,8 @@ function drawCanvasCenteredAt(iDOM, iDrawFunction, iTranslateX = 0, iTranslateY 
 
 function drawArrow(iDOM, iTailWidth, iTailLength, iHeadWidth, iHeadLength) {
 
-  var wTailWidthHalf = Math.abs(iTailWidth/2);
-  var wHeadWidthHalf = Math.abs(iHeadWidth/2);
+  var wTailWidthHalf = Math.abs(iTailWidth / 2);
+  var wHeadWidthHalf = Math.abs(iHeadWidth / 2);
 
   var wCtx = iDOM.getContext("2d");
   wCtx.beginPath();
@@ -90,20 +88,20 @@ function drawComplexFunction(iDOM, iStartTime, iEndTime, iTimeStep, iComplexFunc
 
 function drawPolyLine(iDOM, iPointArray, iXScale = 1.0, iYScale = 1.0, iXOffset = 0, iYOffset = 0) {
 
-  if (iPointArray.length == 0){
+  if (iPointArray.length == 0) {
     return;
   }
 
   var wCtx = iDOM.getContext("2d");
   wCtx.beginPath();
-  
-  var wX = iXScale*iPointArray[0][0] + iXOffset;
-  var wY = iYScale*iPointArray[0][1] + iYOffset;
+
+  var wX = iXScale * iPointArray[0][0] + iXOffset;
+  var wY = iYScale * iPointArray[0][1] + iYOffset;
   wCtx.moveTo(wX, wY);
 
   for (var wi = 0; wi < iPointArray.length; ++wi) {
-    wX = iXScale*iPointArray[wi][0] + iXOffset;
-    wY = iYScale*iPointArray[wi][1] + iYOffset;
+    wX = iXScale * iPointArray[wi][0] + iXOffset;
+    wY = iYScale * iPointArray[wi][1] + iYOffset;
     wCtx.lineTo(wX, wY);
   }
 
@@ -113,20 +111,20 @@ function drawPolyLine(iDOM, iPointArray, iXScale = 1.0, iYScale = 1.0, iXOffset 
 
 function drawPolyLineXY(iDOM, iPointArray, iXScale = 1.0, iYScale = 1.0, iXOffset = 0, iYOffset = 0) {
 
-  if (iPointArray.length == 0){
+  if (iPointArray.length == 0) {
     return;
   }
 
   var wCtx = iDOM.getContext("2d");
   wCtx.beginPath();
-  
-  var wX = iXScale*iPointArray[0].x + iXOffset;
-  var wY = iYScale*iPointArray[0].y + iYOffset;
+
+  var wX = iXScale * iPointArray[0].x + iXOffset;
+  var wY = iYScale * iPointArray[0].y + iYOffset;
   wCtx.moveTo(wX, wY);
 
   for (var wi = 0; wi < iPointArray.length; ++wi) {
-    wX = iXScale*iPointArray[wi].x + iXOffset;
-    wY = iYScale*iPointArray[wi].y + iYOffset;
+    wX = iXScale * iPointArray[wi].x + iXOffset;
+    wY = iYScale * iPointArray[wi].y + iYOffset;
     wCtx.lineTo(wX, wY);
   }
 
@@ -135,69 +133,68 @@ function drawPolyLineXY(iDOM, iPointArray, iXScale = 1.0, iYScale = 1.0, iXOffse
 }
 
 
-function drawNumberLine(iDOM, iStartX, iStartY, iEndX, iEndY, iRefX, iRefY, iRefValue=0, iUnitScale=1, iMajorIncrement=5, iMinorIncrement=1,iMajorLength=2, iMinorLength =1,iMajorLineWidth=2, iMinorLineWidth =1) {
+function drawNumberLine(iDOM, iStartX, iStartY, iEndX, iEndY, iRefX, iRefY, iRefValue = 0, iUnitScale = 1, iMajorIncrement = 5, iMinorIncrement = 1, iMajorLength = 2, iMinorLength = 1, iMajorLineWidth = 2, iMinorLineWidth = 1) {
 
   var wDirection = {
-    x : iEndX - iStartX,
-    y : iEndY - iStartY
+    x: iEndX - iStartX,
+    y: iEndY - iStartY
   }
 
-  var wTotalMag = Math.sqrt(wDirection.x*wDirection.x + wDirection.y*wDirection.y);
+  var wTotalMag = Math.sqrt(wDirection.x * wDirection.x + wDirection.y * wDirection.y);
 
   wDirection.x /= wTotalMag;
   wDirection.y /= wTotalMag;
 
   var wRefDirection = {
-    x : iRefX - iStartX,
-    y : iRefY - iStartY
+    x: iRefX - iStartX,
+    y: iRefY - iStartY
   }
 
-  var wDot = wRefDirection.x*wDirection.x + wRefDirection.y*wDirection.y;
+  var wDot = wRefDirection.x * wDirection.x + wRefDirection.y * wDirection.y;
   var wClosestPointOnLine = {
-    x : wDot*wDirection.x,
-    y : wDot*wDirection.y
+    x: wDot * wDirection.x,
+    y: wDot * wDirection.y
   }
 
-  var wRefMag = Math.sqrt(wClosestPointOnLine.x*wClosestPointOnLine.x + wClosestPointOnLine.y*wClosestPointOnLine.y);
+  var wRefMag = Math.sqrt(wClosestPointOnLine.x * wClosestPointOnLine.x + wClosestPointOnLine.y * wClosestPointOnLine.y);
   var wInvertRefMag = wTotalMag - wRefMag;
   if (wDot <= 0) {
     wInvertRefMag = wTotalMag + wRefMag;
   }
 
   var wPerpendicular = {
-    x : wRefDirection.x - wClosestPointOnLine.x,
-    y : wRefDirection.y - wClosestPointOnLine.y
+    x: wRefDirection.x - wClosestPointOnLine.x,
+    y: wRefDirection.y - wClosestPointOnLine.y
   }
 
-  var wPerpendicularMag = Math.sqrt(wPerpendicular.x*wPerpendicular.x + wPerpendicular.y*wPerpendicular.y);
+  var wPerpendicularMag = Math.sqrt(wPerpendicular.x * wPerpendicular.x + wPerpendicular.y * wPerpendicular.y);
   wPerpendicular.x /= wPerpendicularMag;
   wPerpendicular.y /= wPerpendicularMag;
 
   var wCtx = iDOM.getContext("2d");
 
-  if (0 != iMinorIncrement)
-  {
-    var wPixelJump = Math.abs(iUnitScale)*iMinorIncrement;
+  if (0 != iMinorIncrement) {
+    var wPixelJump = Math.abs(iUnitScale) * iMinorIncrement;
     var wPixelJumpVector = {
-      x : wPixelJump*wDirection.x,
-      y : wPixelJump*wDirection.y
+      x: wPixelJump * wDirection.x,
+      y: wPixelJump * wDirection.y
     }
 
-    var wForwardCount = parseInt(Math.ceil(wInvertRefMag/wPixelJump));
-    var wBackwardCount = parseInt(Math.ceil(wRefMag/wPixelJump));
+    var wForwardCount = parseInt(Math.ceil(wInvertRefMag / wPixelJump));
+    var wBackwardCount = parseInt(Math.ceil(wRefMag / wPixelJump));
 
     var wMinorStartX = wClosestPointOnLine.x + iStartX;
     var wMinorStartY = wClosestPointOnLine.y + iStartY;
-    var wMinorEndX = wClosestPointOnLine.x + iStartX + iMinorLength*wPerpendicular.x;
-    var wMinorEndY = wClosestPointOnLine.y + iStartY + iMinorLength*wPerpendicular.y;
+    var wMinorEndX = wClosestPointOnLine.x + iStartX + iMinorLength * wPerpendicular.x;
+    var wMinorEndY = wClosestPointOnLine.y + iStartY + iMinorLength * wPerpendicular.y;
 
     wCtx.lineWidth = iMinorLineWidth;
-    for ( var wi = -wBackwardCount;  wi <= wForwardCount; ++wi ){
-      var wNewStartX = wMinorStartX + wi*wPixelJumpVector.x;
-      var wNewStartY = wMinorStartY + wi*wPixelJumpVector.y;
-      var wNewEndX = wMinorEndX + wi*wPixelJumpVector.x;
-      var wNewEndY = wMinorEndY + wi*wPixelJumpVector.y;
-      
+    for (var wi = -wBackwardCount; wi <= wForwardCount; ++wi) {
+      var wNewStartX = wMinorStartX + wi * wPixelJumpVector.x;
+      var wNewStartY = wMinorStartY + wi * wPixelJumpVector.y;
+      var wNewEndX = wMinorEndX + wi * wPixelJumpVector.x;
+      var wNewEndY = wMinorEndY + wi * wPixelJumpVector.y;
+
       wCtx.beginPath();
       wCtx.moveTo(wNewStartX, wNewStartY);
       wCtx.lineTo(wNewEndX, wNewEndY);
@@ -205,37 +202,36 @@ function drawNumberLine(iDOM, iStartX, iStartY, iEndX, iEndY, iRefX, iRefY, iRef
     }
   }
 
-  if (0 != iMajorIncrement)
-  {
-    var wPixelJump = Math.abs(iUnitScale)*iMajorIncrement;
+  if (0 != iMajorIncrement) {
+    var wPixelJump = Math.abs(iUnitScale) * iMajorIncrement;
     var wPixelJumpVector = {
-      x : wPixelJump*wDirection.x,
-      y : wPixelJump*wDirection.y
+      x: wPixelJump * wDirection.x,
+      y: wPixelJump * wDirection.y
     }
 
-    var wForwardCount = parseInt(Math.ceil(wInvertRefMag/wPixelJump));
-    var wBackwardCount = parseInt(Math.ceil(wRefMag/wPixelJump));
+    var wForwardCount = parseInt(Math.ceil(wInvertRefMag / wPixelJump));
+    var wBackwardCount = parseInt(Math.ceil(wRefMag / wPixelJump));
 
     var wMajorStartX = wClosestPointOnLine.x + iStartX;
     var wMajorStartY = wClosestPointOnLine.y + iStartY;
-    var wMajorEndX = wClosestPointOnLine.x + iStartX + iMajorLength*wPerpendicular.x;
-    var wMajorEndY = wClosestPointOnLine.y + iStartY + iMajorLength*wPerpendicular.y;
+    var wMajorEndX = wClosestPointOnLine.x + iStartX + iMajorLength * wPerpendicular.x;
+    var wMajorEndY = wClosestPointOnLine.y + iStartY + iMajorLength * wPerpendicular.y;
 
     wCtx.lineWidth = iMajorLineWidth;
-    for ( var wi = -wBackwardCount;  wi <= wForwardCount; ++wi ){
-      var wNewStartX = wMajorStartX + wi*wPixelJumpVector.x;
-      var wNewStartY = wMajorStartY + wi*wPixelJumpVector.y;
-      var wNewEndX = wMajorEndX + wi*wPixelJumpVector.x;
-      var wNewEndY = wMajorEndY + wi*wPixelJumpVector.y;
-      
+    for (var wi = -wBackwardCount; wi <= wForwardCount; ++wi) {
+      var wNewStartX = wMajorStartX + wi * wPixelJumpVector.x;
+      var wNewStartY = wMajorStartY + wi * wPixelJumpVector.y;
+      var wNewEndX = wMajorEndX + wi * wPixelJumpVector.x;
+      var wNewEndY = wMajorEndY + wi * wPixelJumpVector.y;
+
       wCtx.beginPath();
       wCtx.moveTo(wNewStartX, wNewStartY);
       wCtx.lineTo(wNewEndX, wNewEndY);
       wCtx.stroke();
-      
-      wCtx.textAlign='center';
+
+      wCtx.textAlign = 'center';
       wCtx.textBaseline = 'middle';;
-      wCtx.fillText("" + (parseInt(iRefValue) + wi*iMajorIncrement),iRefX + wi*wPixelJumpVector.x,iRefY + wi*wPixelJumpVector.y);
+      wCtx.fillText("" + (parseInt(iRefValue) + wi * iMajorIncrement), iRefX + wi * wPixelJumpVector.x, iRefY + wi * wPixelJumpVector.y);
     }
   }
 
@@ -245,4 +241,3 @@ function drawNumberLine(iDOM, iStartX, iStartY, iEndX, iEndY, iRefX, iRefY, iRef
   wCtx.lineTo(iEndX, iEndY);
   wCtx.stroke();
 }
--->
