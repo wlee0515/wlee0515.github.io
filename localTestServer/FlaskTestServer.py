f5ce276730eea41d92a8ac61ebec6c4daa4d9f4a
Willee <wilson.lee@cae.com>
2020-02-14 18:20:52 -0500

    Added Main Loop to local test  server

--- localTestServer/FlaskTestServer.py
+++ localTestServer/FlaskTestServer.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, send_from_directory
 import os
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__, static_url_path='/')
 
@@ -12,6 +15,17 @@ def send_site(path):
     return send_from_directory('../site', path)
 
 
-if __name__ == '__main__':
+def iteration():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+def main():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=iteration, trigger="interval", seconds=5)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+if __name__ == '__main__':
+    main()
